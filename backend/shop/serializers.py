d7450c55aad6828d4b0e0f0df5313e7060b20735
Kenthyvuth <kenthyvuth.tan@gmail.com>
2025-06-10 13:00:32 +0200

    fix save user password in database

--- backend/shop/serializers.py
+++ backend/shop/serializers.py
@@ -11,6 +11,16 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User(
+            username=validated_data['username'],
+            email=validated_data['email']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
 
 class OrderItemSerializer(serializers.ModelSerializer):
     product = ProductSerializer()
