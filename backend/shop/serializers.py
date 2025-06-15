4aa6cb56a1ebb6379a88d41dc82285b4a7ddc298
Kenthyvuth <kenthyvuth.tan@gmail.com>
2025-06-15 13:39:54 +0200

    feature validate cart

--- backend/shop/serializers.py
+++ backend/shop/serializers.py
@@ -23,16 +23,24 @@ class UserSerializer(serializers.ModelSerializer):
         return user
 
 class OrderItemSerializer(serializers.ModelSerializer):
-    product = ProductSerializer()
+    product = serializers.PrimaryKeyRelatedField(queryset=Product.objects.all())
     class Meta:
         model = OrderItem
         fields = ('product', 'qty', 'price')
 
 class OrderSerializer(serializers.ModelSerializer):
     items = OrderItemSerializer(many=True)
+
     class Meta:
         model = Order
         fields = ('id', 'user', 'total', 'created_at', 'items')
+
+    def create(self, validated_data):
+        items_data = validated_data.pop('items')
+        order = Order.objects.create(**validated_data)
+        for item_data in items_data:
+            OrderItem.objects.create(order=order, **item_data)
+        return order
 
 class ContactSerializer(serializers.ModelSerializer):
     class Meta:
