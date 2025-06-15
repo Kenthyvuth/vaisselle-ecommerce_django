f272d656b49b7890979279b5a12f5f048dd4b5dc
Kenthyvuth <kenthyvuth.tan@gmail.com>
2025-06-15 13:40:25 +0200

    admin view

--- backend/shop/admin.py
+++ backend/shop/admin.py
@@ -1,7 +1,25 @@
 from django.contrib import admin
 from .models import Product, Order, OrderItem, Contact
 
-admin.site.register(Product)
-admin.site.register(Order)
-admin.site.register(OrderItem)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock', 'category')
+    search_fields = ('name', 'category')
+
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('order', 'product', 'qty', 'price')
+    readonly_fields = ('order', 'product', 'qty', 'price')
+    search_fields = ('order__id', 'product__name')
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('command_id', 'user', 'created_at', 'total')
+    readonly_fields = ('user', 'created_at', 'total')
+    search_fields = ('user__username',)
+
+    def command_id(self, obj):
+        return obj.pk
+    command_id.short_description = 'Order ID'
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderItem, OrderItemAdmin)
 admin.site.register(Contact)
